b729109c95bed5d3c5b48bde2031387f0ff7e8f8
codebinarysolutions <codebnrysolutions@gmail.com>
2025-03-10 23:06:22 -0700

    Initial Version
    Initial Version

---
+++ JokeHub/src/app/hello/hello.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.scss']
+})
+export class HelloComponent {
+    message: string = 'Hello, Angular! 🎉';
+    message2 : string = 'This is my added message !!';
+    changeMessage()
+    {
+      this.message = 'You clicked the button !🚀';
+    }
+  
+    joke : string = 'Click to Joke';
+    changeMessage2()
+    {
+      this.joke = 'You clicked the button  2 !🚀';
+    }
+    constructor(private http: HttpClient) {}
+
+   fetchJoke() {
+      this.http.get<any>('https://official-joke-api.appspot.com/random_joke')
+        .subscribe(response => {
+          this.joke = `${response.setup} - ${response.punchline}`;
+        });
+   }
+}
